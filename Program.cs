14bccc471f98ff0f0b223ac72abf3f2c54d12ac6
Joshua Leal <joshnleal@gmail.com>
2025-09-09 16:05:26 +0200

    Added an end for the game.

--- Program.cs
+++ Program.cs
@@ -61,32 +61,35 @@ Console.WriteLine("Battle Phase");
 Console.WriteLine("------------");
 Random random = new Random();
 int rng;
-foreach (int monsterHealth in monsters) {
+int killCount = 0;
+foreach (int monsterHealth in monsters)
+{
     int currentMonsterHealth = monsterHealth;
     if (playerHealth <= 0)
         break;
     else
         Console.WriteLine("A monster appears...");
     while (!(currentMonsterHealth <= 0) && !(playerHealth <= 0))
-        {
-            Console.WriteLine("Current Health");
-            Console.WriteLine($"{heroName}: {playerHealth}");
-            Console.WriteLine($"Monster: {currentMonsterHealth}");
-            Console.WriteLine($"\n{heroName}'s turn");
-            Console.WriteLine("Select an action");
-            Console.WriteLine("1. Heal");
-            Console.WriteLine("2. Attack");
-            string? input = null;
-            int playerChoice;
+    {
+        Console.WriteLine("Current Health");
+        Console.WriteLine($"{heroName}: {playerHealth}");
+        Console.WriteLine($"Monster: {currentMonsterHealth}");
+        Console.WriteLine($"\n{heroName}'s turn");
+        Console.WriteLine("Select an action");
+        Console.WriteLine("1. Heal");
+        Console.WriteLine("2. Attack");
+        string? input = null;
+        int playerChoice;
         try
         {
+            Console.Write("Input:");
             input = Console.ReadLine();
-    
+
             if (!int.TryParse(input, out playerChoice))
                 throw new ArgumentException("Error: Input an integer.");
             if (playerChoice < 1 || playerChoice > 2)
                 throw new ArgumentException("Error: Input must be 1 or 2.");
-    
+
             rng = random.Next(5, 16);
 
             if (playerChoice == 1)
@@ -95,26 +98,38 @@ foreach (int monsterHealth in monsters) {
                 Console.WriteLine($"{heroName} heals for {rng} hit points.");
             }
             else
-            { 
+            {
                 currentMonsterHealth -= rng;
                 Console.WriteLine($"{heroName} attacks monsters for {rng} hit points.");
             }
 
             if (currentMonsterHealth <= 0)
             {
-                Console.WriteLine("Monster defeated!");
+                Console.WriteLine("Monster defeated!\n");
+                killCount++;
             }
             else
             {
                 rng = random.Next(5, 16);
                 playerHealth -= rng;
-                Console.WriteLine($"Monster attacks {heroName} for {rng} hit points.");    
+                Console.WriteLine($"Monster attacks {heroName} for {rng} hit points.");
             }
-            }
+        }
         catch (ArgumentException ex)
         {
             Console.WriteLine(ex.Message);
         }
+    }
+}
 
-        }
+
+if (playerHealth < 0)
+{
+    Console.WriteLine("Game Over");
+    Console.WriteLine($"{heroName} killed in action... {killCount} monster(s) slain.");
+}
+else
+{
+    Console.WriteLine("Congratulations!");
+    Console.WriteLine("You killed all the monsters!");
 }
