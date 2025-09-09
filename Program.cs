8909a640dd366a881ae7d838e5b36ebf3ce82f58
Joshua Leal <joshnleal@gmail.com>
2025-09-09 14:38:42 +0200

    Created initial part of the program (Asking for player name)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome");
+string? heroName = null;
+int monsterCount = 0;
+do
+{
+    try
+    {
+        Console.Write("Enter your name: ");
+        heroName = Console.ReadLine();
+        int numericName = -1;
+        if (heroName == "")
+        {
+            heroName = "Hero";
+        }
+        else if (int.TryParse(heroName, out numericName))
+        {
+            throw new ArgumentException("Error: Name must contain characters.");
+        }
+    }
+    catch (ArgumentException ex)
+    {
+        Console.WriteLine(ex.Message);
+        heroName = null;
+    }
+
+} while (heroName is null);
