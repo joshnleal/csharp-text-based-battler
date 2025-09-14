d9d7af6d3b76c36f345323e0e22c9def73796579
Joshua Leal <joshnleal@gmail.com>
2025-09-14 17:13:24 +0200

    Changed logic of instantiating Weapon object. Added list of weapons with associated name, strength, reliability, and weight along with a list of adjectives to describe the rarity of the weapon.

--- Battler/Weapon.cs
+++ Battler/Weapon.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace Battler {
     public class Weapon
     {
@@ -8,10 +10,37 @@ namespace Battler {
 
         public Weapon(string name, int strength, int reliability, int weight)
         {
-            _name = name;
-            _strength = strength;
-            _reliability = reliability;
-            _weight = weight;
+            // [(noun, strength, reliability, weight,)...]
+            var nouns = new[] {
+                new { Name = "sword", Strength = 4, Reliability = 8, Weight = 5 },
+                new { Name = "shield", Strength = 1, Reliability = 8, Weight = 8 },
+                new { Name = "bow", Strength = 8, Reliability = 2, Weight = 3 },
+                new { Name = "canon", Strength = 9, Reliability = 2, Weight = 15 },
+                new { Name = "force field", Strength = 2, Reliability = 7, Weight = 1 },
+                new { Name = "drone", Strength = 9, Reliability = 2, Weight = 20 },
+                new { Name = "tank", Strength = 9, Reliability = 7, Weight = 45 }
+            };
+
+            // [(adjective, multiplier,)...]
+            var adjectives = new[] {
+                new { Adjective = "wooden", Multiplier = 1 },
+                new { Adjective = "bronze", Multiplier = 2 },
+                new { Adjective = "steel", Multiplier = 3 },
+                new { Adjective = "diamond", Multiplier = 4 },
+                new { Adjective = "nanofiber", Multiplier = 5 },
+                new { Adjective = "unobtainium", Multiplier = 6 },
+                new { Adjective = "epic", Multiplier = 7 },
+                new { Adjective = "legendary", Multiplier = 8 },
+                new { Adjective = "eternal", Multiplier = 9 },
+            };
+
+            Random random = new();
+            var noun = nouns[random.Next(0, nouns.Length)];
+            var adjective = adjectives[random.Next(0,  adjectives.Length)];
+            _name = adjective.Adjective + noun.Name;
+            _strength = noun.Strength;
+            _reliability = noun.Reliability;
+            _weight = noun.Weight;
         }
 
         public string Name
